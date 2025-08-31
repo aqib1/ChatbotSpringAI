282bb35b469b37fe9b9d9b9da8cbbbc5b8f8b3f3
aqib javed <aqibbutt3078@gmail.com>
2025-08-31 01:02:03 +0100

    Prompt from resources

--- src/main/java/com/aifirst/io/chatbotspringai/controller/HelloController.java
+++ src/main/java/com/aifirst/io/chatbotspringai/controller/HelloController.java
@@ -1,16 +1,23 @@
 package com.aifirst.io.chatbotspringai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("chat")
 public class HelloController {
+    @Value("classpath:/prompts/celeb-details.st")
+    private Resource celebPrompt;
     private final ChatClient chatClient;
 
     public HelloController(ChatClient.Builder builder) {
@@ -28,6 +35,21 @@ public class HelloController {
                 .prompt(message)
                 .call()
                 .chatResponse()).orElseThrow(() -> new RuntimeException(""));
+
+        return response.getResult()
+                .getOutput()
+                .getText();
+    }
+
+    @GetMapping("/celeb")
+    public String getCeleb(@RequestParam String celeb) {
+        PromptTemplate template = new PromptTemplate(celebPrompt);
+        Prompt prompt = template.create(Map.of("name", celeb));
+        var response = Optional.ofNullable(
+                chatClient.prompt(prompt)
+                        .call()
+                        .chatResponse()
+        ).orElseThrow(() -> new RuntimeException(""));
 
         return response.getResult()
                 .getOutput()
