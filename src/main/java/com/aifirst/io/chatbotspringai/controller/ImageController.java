67263e5a6a7ba8a08f2ec9c4c0ba2db93c2e9d1e
aqib javed <aqibbutt3078@gmail.com>
2025-09-05 01:02:03 +0100

    Create Image from prompt

--- src/main/java/com/aifirst/io/chatbotspringai/controller/ImageController.java
+++ src/main/java/com/aifirst/io/chatbotspringai/controller/ImageController.java
@@ -2,9 +2,13 @@ package com.aifirst.io.chatbotspringai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageController {
 
     private final ChatModel chatModel;
+    private final ImageModel imageModel;
 
-    public ImageController(ChatModel chatModel) {
+    public ImageController(ChatModel chatModel, ImageModel imageModel) {
         this.chatModel = chatModel;
+        this.imageModel = imageModel;
     }
 
     @GetMapping("/to-text")
@@ -31,5 +37,23 @@ public class ImageController {
                 ).call()
                 .content();
 
+    }
+
+    // Using DALL-E model
+    @GetMapping("/to-image/{prompt}")
+    public String generateImage(@PathVariable String prompt) {
+       var imageResponse = imageModel.call(
+                new ImagePrompt(
+                        prompt,
+                        OpenAiImageOptions.builder()
+                                .N(1)
+                                .width(1024)
+                                .height(1024)
+                                .quality("hd")
+                                .build()
+                )
+        );
+
+       return imageResponse.getResult().getOutput().getUrl();
     }
 }
