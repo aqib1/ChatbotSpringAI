85370d06ffc44db254965321a263e886620203d8
aqib javed <aqibbutt3078@gmail.com>
2025-09-07 01:02:03 +0100

    Text to speech

--- src/main/java/com/aifirst/io/chatbotspringai/controller/AudioController.java
+++ src/main/java/com/aifirst/io/chatbotspringai/controller/AudioController.java
@@ -1,23 +1,37 @@
 package com.aifirst.io.chatbotspringai.controller;
 
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("audio")
 public class AudioController {
     private final OpenAiAudioTranscriptionModel audioTranscriptionModel;
-    public AudioController(OpenAiAudioTranscriptionModel audioTranscriptionModel) {
+    private final OpenAiAudioSpeechModel audioSpeechModel;
+
+    public AudioController(
+            OpenAiAudioTranscriptionModel audioTranscriptionModel,
+            OpenAiAudioSpeechModel audioSpeechModel
+    ) {
         this.audioTranscriptionModel = audioTranscriptionModel;
+        this.audioSpeechModel = audioSpeechModel;
     }
 
-    @GetMapping("/to-text")
+    @GetMapping("/audio-to-text")
     public String audioTranscription() {
         var options = OpenAiAudioTranscriptionOptions
                 .builder()
@@ -34,5 +48,29 @@ public class AudioController {
                 .call(prompt)
                 .getResult()
                 .getOutput();
+    }
+
+    @GetMapping("/text-to-audio/{prompt}")
+    public ResponseEntity<Resource> generateAudio(@PathVariable String prompt) {
+        var options = OpenAiAudioSpeechOptions.builder()
+                .model(OpenAiAudioApi.TtsModel.TTS_1.getValue())
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .voice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+                .speed(1.0f)
+                .build();
+        var speechPrompt = new SpeechPrompt(prompt, options);
+        var response = audioSpeechModel.call(speechPrompt);
+        var output = response.getResult().getOutput();
+        var byteArrayResource = new ByteArrayResource(output);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(byteArrayResource.contentLength())
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        ContentDisposition.attachment()
+                                .filename("test.mp3")
+                                .build().toString())
+                .body(byteArrayResource);
+
+
     }
 }
